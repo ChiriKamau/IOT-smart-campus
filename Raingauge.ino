3d05433e8c8e550887d3bbaedb756794c6c4be5b
Levis Chiri <120241319+ChiriKamau@users.noreply.github.com>
2023-05-25 17:52:05 +0300

    Add files via upload

---
+++ Raingauge.ino
@@ -0,0 +1,54 @@
+float pingPin = 2; 
+float echoPin = 3; 
+float maxDistance=17.79;
+float area= 59.3;
+float Sarea = 47.1;
+float volume;
+void setup() {
+   Serial.begin(9600); 
+   pinMode(pingPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+}
+
+
+void loop() {
+
+water_volume();
+   Serial.print(volume);
+   Serial.print("cm");
+   Serial.println();
+   delay(2000);
+}
+
+
+
+ void water_volume(){
+  
+      float duration, waterLevel,waterHeight;
+
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   
+   waterLevel = microsecondsToCentimeters(duration);
+  waterHeight= maxDistance-waterLevel;
+  if(waterHeight<1.5){
+     waterHeight= maxDistance-waterLevel;
+    volume = waterHeight*Sarea;
+  }
+  else{
+ waterHeight= maxDistance-1.5-waterLevel;
+volume= (area*waterHeight)+50;
+
+  }
+
+ }
+float microsecondsToCentimeters(float microseconds) {
+   return microseconds / 29 / 2;
+}
