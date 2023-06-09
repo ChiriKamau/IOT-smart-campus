8a553f9aaa371e75a982a75bbebc47dd13edba95
Ryan-py <71486185+Ryan-py@users.noreply.github.com>
2023-06-09 20:38:36 +0300

    Update thingProperties.h

--- deployment_code/thingProperties.h
+++ deployment_code/thingProperties.h
@@ -3,7 +3,7 @@
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
 
-const char DEVICE_LOGIN_NAME[]  = "baf33a57-1fd8-4b03-84a6-b2978cb55818";
+const char DEVICE_LOGIN_NAME[]  = "paste here";
 
 const char SSID[]               = SECRET_SSID;    // Network SSID (name)
 const char PASS[]               = SECRET_OPTIONAL_PASS;    // Network password (use for WPA, or use as key for WEP)
